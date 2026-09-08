27f3f84fbfdfd1711afad08b1cf151b5ab71e60d
Harshit Yadav <harshityadav@outlook.com>
2026-09-09 00:22:12 +0530

    Update Mac Noodle to 1.0.4 (38)

--- Casks/mac-noodle.rb
+++ Casks/mac-noodle.rb
@@ -1,11 +1,19 @@
 cask "mac-noodle" do
-  version "1.0.3,35"
-  sha256 "9aede20f4673ef10c02e7fb91735c7c0c7bd4e96053979ec9d507d81ca303fb2"
+  version "1.0.4,38"
+  sha256 "0834eb57dbb13b0252a2ad35cbf7665751c0e4144308cb454745975adc29ce89"
 
-  url "https://github.com/solvePao/Mac-Noodle-Web/releases/download/v1.0.3-xcode-cloud.35/Mac-Noodle.dmg"
+  url "https://github.com/solvePao/Mac-Noodle-Web/releases/download/v1.0.4-xcode-cloud.38/Mac-Noodle.dmg"
   name "Mac Noodle"
   desc "Free space safely with guided cleanup tools"
   homepage "https://github.com/solvePao/Mac-Noodle-Web"
 
+  depends_on macos: ">= :ventura"
+
   app "Mac Noodle.app"
+
+  postflight do
+    marker = "#{Dir.home}/Library/Containers/com.solvepao.mac.noodle/Data/Library/Application Support/Mac Noodle/distribution-channel"
+    FileUtils.mkdir_p(File.dirname(marker))
+    File.write(marker, "homebrew-v1\n")
+  end
 end
