f73d36bf545d29eacfe82b577c3f0464551085d7
Harshit Yadav <harshityadav@outlook.com>
2026-09-09 01:12:44 +0530

    Update Mac Noodle to 1.0.5 (46)

--- Casks/mac-noodle.rb
+++ Casks/mac-noodle.rb
@@ -1,11 +1,11 @@
 cask "mac-noodle" do
-  version "1.0.4,38"
-  sha256 "0834eb57dbb13b0252a2ad35cbf7665751c0e4144308cb454745975adc29ce89"
+  version "1.0.5,46"
+  sha256 "7d7b17c3c0936545de6f295264f18250be09239fea6a25a02719d71c9f5ba6ec"
 
-  url "https://github.com/solvePao/Mac-Noodle-Web/releases/download/v1.0.4-xcode-cloud.38/Mac-Noodle.dmg"
+  url "https://github.com/solvePao/Mac-Noodle-Web/releases/download/v1.0.5-xcode-cloud.46/Mac-Noodle.dmg"
   name "Mac Noodle"
-  desc "Free space safely with guided cleanup tools"
-  homepage "https://github.com/solvePao/Mac-Noodle-Web"
+  desc "A beautiful macOS menu bar app for visualizing disk usage"
+  homepage "https://macnoodle.com"
 
   depends_on macos: ">= :ventura"
 
@@ -14,6 +14,11 @@ cask "mac-noodle" do
   postflight do
     marker = "#{Dir.home}/Library/Containers/com.solvepao.mac.noodle/Data/Library/Application Support/Mac Noodle/distribution-channel"
     FileUtils.mkdir_p(File.dirname(marker))
-    File.write(marker, "homebrew-v1\n")
+    File.write(marker, "homebrew-v1\\n")
   end
+
+  zap trash: [
+    "~/Library/Application Support/Mac Noodle",
+    "~/Library/Containers/com.solvepao.mac.noodle",
+  ]
 end
